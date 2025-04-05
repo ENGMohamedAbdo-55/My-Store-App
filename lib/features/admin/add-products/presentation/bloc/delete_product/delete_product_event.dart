60c86755ba9442e8e2314ee21078966b06f016f0
Mohamed Abdo <eng.mohamedabdo139@gmail.com>
2025-04-05 07:11:51 +0200

    feat: remove delete product BLoC event and state management files

--- lib/features/admin/add-products/presentation/bloc/delete_product/delete_product_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'delete_product_bloc.dart';
-
-@freezed
-class DeleteProductEvent with _$DeleteProductEvent {
-  const factory DeleteProductEvent.started() = _Started;
-}
--- lib/features/admin/add-products/presentation/bloc/delete_product/delete_product_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'delete_product_bloc.dart';
-
-@freezed
-class DeleteProductState with _$DeleteProductState {
-  const factory DeleteProductState.initial() = _Initial;
-  const factory DeleteProductState.loading() = LoadingState;
-  const factory DeleteProductState.success() = SuccessState;
-  const factory DeleteProductState.failure({required String errorMessage}) = FailureState;
-}
